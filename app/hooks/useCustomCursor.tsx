811435100c7b6050c7cd65e2e0fd6ed58835e9f5
numanjvd82 <nomanmani62@gmail.com>
2024-11-09 23:10:20 +0500

    fixed the initial cursor position on refresh

--- app/hooks/useCustomCursor.tsx
+++ app/hooks/useCustomCursor.tsx
@@ -28,9 +28,16 @@ export default function useCustomCursor() {
   };
 
   useEffect(() => {
+    const initialX = window.innerWidth / 2;
+    const initialY = window.innerHeight / 2;
+    mousePosition.x.set(initialX - CURSOR_SIZE / 2);
+    mousePosition.y.set(initialY - CURSOR_SIZE / 2);
+
     window.addEventListener("mousemove", handleMouseMove);
 
-    return () => window.removeEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
   });
 
   return {
